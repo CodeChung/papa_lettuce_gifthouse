96b3660456e3b30206d0502dc0b58f0bafe0a2dd
Harry Chung <harrychung@Harrys-MBP.fios-router.home>
2021-12-12 23:34:50 -0500

    converted divs to react-router links

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ import menuIcon from './img/icons/menu.png';
 import newsIcon from './img/icons/newsbug.png';
 import cartIcon from './img/icons/checkout.png';
 import aboutIcon from './img/icons/about.png';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
@@ -18,31 +25,31 @@ function App() {
       </header>
       <body>
         <div className="Menu-container">
-          <div className="Menu-item" id="Menu-item-menu">
+          <Link to='/menu' className="Menu-item" id="Menu-item-menu">
             <div className='menu-title'><h4>Menu</h4></div>
             <div className='menu-subtitle'><h6>Check out our gifts & goodies</h6></div>
             <img className='menu-icon' src={menuIcon} alt='you used to call me on my cell phone. now you must text' />
-          </div>
-          <div className="Menu-item" id="Menu-item-news">
+          </Link>
+          <Link to='/news' className="Menu-item" id="Menu-item-news">
             <div className='menu-title'><h4>News</h4></div>
             <div className='menu-subtitle'><h6>Extra, extra read all about it</h6></div>
             <img className='menu-icon' src={newsIcon} alt='you used to call me on my cell phone. now you must text' />
-          </div>
-          <div className="Menu-item" id="Menu-item-about">
+          </Link>
+          <Link to='/about' className="Menu-item" id="Menu-item-about">
             <div className='menu-title'><h4>About</h4></div>
             <div className='menu-subtitle'><h6>Let me tell you about me, young blood</h6></div>
             <img className='menu-icon' src={aboutIcon} alt='you used to call me on my cell phone. now you must text' />
-          </div>
-          <div className="Menu-item" id="Menu-item-contact">
+          </Link>
+          <Link to='/contact' className="Menu-item" id="Menu-item-contact">
             <div className='menu-title'><h4>Contact</h4></div>
             <div className='menu-subtitle'><h6>You used 2 call me on my cell phone</h6></div>
             <img className='menu-icon' src={cannaPhoneIcon} alt='you used to call me on my cell phone. now you must text' />
-          </div>
-          <div className="Menu-item" id="Menu-item-cart">
+          </Link>
+          <Link to='/cart' className="Menu-item" id="Menu-item-cart">
             <div className='menu-title'><h4>Cart</h4></div>
             <div className='menu-subtitle'><h6>Check yorself b4 you wreck yoself</h6></div>
             <img className='menu-icon' src={cartIcon} alt='you used to call me on my cell phone. now you must text' />
-          </div>
+          </Link>
         </div>
       </body>
       <footer>
